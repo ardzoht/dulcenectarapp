7d189a70a276ad6ae2b9a27555fcc0d2eded4bd4
Alejandro Rodríguez <ardzoht@gmail.com>
2016-10-07 14:26:06 -0600

    updated accessToken

--- public/js/js/index.js
+++ public/js/js/index.js
@@ -1,6 +1,14 @@
 var feed = new Instafeed({
 	get:'user',
-  userId: '6719060',
-  accessToken: '6719060.1677ed0.dfba186d4fe94487801c25abf8615978'
+  userId: '3284850548',
+  accessToken: '3284850548.1677ed0.26b34edc44fe47a5922cd17a26211ba6',
+	sortBy: 'most-recent',
+	resolution: 'low-resolution',
+	after: () => {
+		console.log("Images retrieved");
+	},
+	error: (err) => {
+		console.log(err);
+	}
 });
 feed.run();
