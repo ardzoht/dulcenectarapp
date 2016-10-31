f040cb0924ad4d8448e95038d43631544a80448b
Alejandro Rodríguez <ardzoht@gmail.com>
2016-10-30 19:26:15 -0700

    added DetoxItem, Program models

---
+++ models/DetoxItem.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Alejandro on 30/10/2016.
+ */
+/**
+ * Created by Alejandro on 30/10/2016.
+ */
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+var DetoxItem = new keystone.List('DetoxItem', {
+	autokey: {path: 'slug', from: 'name', unique: true},
+	map: {name: 'name'},
+	defaultSort: '-createdAt'
+});
+
+DetoxItem.add({
+	name: {type: String, required: true},
+	createdAt: {type: Date, default: Date.now},
+	description: {type: String},
+	image: {type: Types.CloudinaryImage},
+	program: {type: Types.Relationship, ref: 'Program'}
+});
+
+DetoxItem.defaultColumns = 'name, createdAt, description, image, program';
+DetoxItem.register();
---
+++ models/Program.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Alejandro on 30/10/2016.
+ */
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+var Program = new keystone.List('DetoxItem', {
+	autokey: {path: 'slug', from: 'name', unique: true},
+	map: {name: 'name'},
+	defaultSort: '-createdAt'
+});
+
+Program.add({
+	name: {type: String, required: true},
+	createdAt: {type: Date, default: Date.now},
+	description: {type: String},
+	image: {type: Types.CloudinaryImage},
+	items: {type: Types.Relationship, ref: 'DetoxItem', many:true}
+});
+
+Program.defaultColumns = 'name, createdAt, description, image, items';
+Program.register();
