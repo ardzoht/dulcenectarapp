d2c0bd53d1a9731a6d5db326950c17f9c09ed8f1
Alejandro Rodríguez <ardzoht@gmail.com>
2016-11-03 23:40:16 -0700

    added program page, pending item page for each detox item

--- routes/views/detox.js
+++ routes/views/detox.js
@@ -11,6 +11,24 @@ exports = module.exports = function (req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'detox';
+	
+	locals.data = {
+		programs: [],
+		items: []
+	};
+	
+	view.on('init', function(next) {
+		console.log("hi");
+		keystone.list('Program').model.find().sort('name').exec(function(err, results){
+			if (err || !results.length) {
+				return next(err);
+			}
+			
+			locals.data.programs = results;
+			console.log(results);
+			next(err);
+		})
+	});
 
 	// Render the view
 	view.render('detox');
