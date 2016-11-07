6402bacfb5d34c20d1dbd2ba0453f90c3d11d65a
Alejandro Rodríguez <ardzoht@gmail.com>
2016-11-06 21:52:03 -0700

    fixed faq

--- public/js/js/index.js
+++ public/js/js/index.js
@@ -20,3 +20,16 @@ $( function() {
 	});
 } );
 
+function cycleImages(){
+	var $active = $('#slider .active');
+	var $next = ($active.next().length > 0) ? $active.next() : $('#slider img:first');
+	$next.css('z-index',2);//move the next image up the pile
+	$active.fadeOut(300,function(){//fade out the top image
+		$active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+		$next.css('z-index',3).addClass('active');//make the next image the top one
+	});
+}
+
+$(document).ready(function(){
+	setInterval('cycleImages()', 3500);
+});
