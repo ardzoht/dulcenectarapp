a39669d115b2c61999342e57c390f795681f15eb
Alejandro Rodríguez <ardzoht@gmail.com>
2016-11-16 23:05:08 -0700

    added dynamic change of detox items

--- public/js/js/index.js
+++ public/js/js/index.js
@@ -21,9 +21,18 @@ function loadAccordion() {
 	} );
 }
 
-
+function cycleImages(){
+	var $active = $('#slider .active');
+	var $next = ($active.next().length > 0) ? $active.next() : $('#slider img:first');
+	$next.css('z-index',2);//move the next image up the pile
+	$active.fadeOut(300,function(){//fade out the top image
+		$active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+		$next.css('z-index',3).addClass('active');//make the next image the top one
+	});
+}
 
 function loadDetoxSlider() {
+	setInterval('cycleImages()', 3500);
 	$('.slider-images').slick({
 		dots:true,
 		infinite: true,
