2e93da21a7029c72da5e1b85d7bdcf17a1331f15
Alejandro Rodriguez <ardzoht@gmail.com>
2016-12-08 00:35:22 -0700

    added modal, images

--- public/js/js/index.js
+++ public/js/js/index.js
@@ -88,6 +88,34 @@ function onChangeDetoxItem() {
 	});
 }
 
+function loadModal() {
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on the button, open the modal 
+	window.onload = function() {
+		modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+}
+
 $(window).ready(function () {
 	loadAccordion();
 	loadDetoxSlider();
@@ -96,6 +124,7 @@ $(window).ready(function () {
 	}
 	console.log(top.location.pathname.substr(0));
 	if (top.location.pathname.substr(0) === '/') {
+		loadModal();
 		loadInstafeed();
 	}
 	onChangeDetoxItem();
